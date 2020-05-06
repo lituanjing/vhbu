0efd75e73c09d946273a41741ad74f54f42b4dc5
lituanjing <tuanjing@outlook.com>
2020-05-06 13:46:39 +0800

    拆分 Money.tsx 的代码做到每个文件长度不超过100行

---
+++ src/views/Money/CategorySection.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const CategorySection = styled.section`
+  font-size: 24px;
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    > li { width: 50%; text-align: center; padding: 16px 0;
+      position: relative;
+      &.selected:after { content: ''; display: block; height: 3px;
+        width: 100%; background: #333; position: absolute; bottom: 0;
+        left: 0; }
+    }
+  }
+`;
+
+export { CategorySection }
