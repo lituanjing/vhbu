d9f4af6af0dd85a00b13ead1e948fd26c89de80a
lituanjing <tuanjing@outlook.com>
2020-05-14 15:01:32 +0800

    重构：将 tag: string 重构为 tag: { id: number, name: string }

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -46,9 +46,9 @@ const Tags = () => {
     <Layout>
       <TagList>
         {tags.map(tag =>
-          <li key={tag}>
-            <Link to={'/tags/' + tag}>
-              <span className="oneLine">{tag}</span>
+          <li key={tag.id}>
+            <Link to={'/tags/' + tag.name}>
+              <span className="oneLine">{tag.name}</span>
               <Icon name="right"/>
             </Link>
           </li>
