d557470888b551d68cffe0dedbce0e60bd82ccb8
lituanjing <tuanjing@outlook.com>
2020-05-14 18:41:39 +0800

    优化删除和更新tag的代码

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -22,16 +22,10 @@ const useTags = () => {
     return result;
   };
   const updateTag = (id: number, obj: { name: string }) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1, {id, ...obj});
-    setTags(tagsClone);
+    setTags(tags.map(tag => tag.id === id ? {id, ...obj} : tag));
   };
   const deleteTag = (id: number) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   };
   return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
 };
