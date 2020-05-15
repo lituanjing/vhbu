0a80dee91d535cb6e134ae267ffc56c2711e0e80
lituanjing <tuanjing@outlook.com>
2020-05-15 19:25:09 +0800

    deploy

---
+++ precache-manifest.25cbdc3779dc4e03aaf7c02ec7c7b47d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb7f9bd4124778300f4a75308715122",
+    "url": "/vhbu/index.html"
+  },
+  {
+    "revision": "19f07b0e82c20c81d1de",
+    "url": "/vhbu/static/css/main.09b0f97b.chunk.css"
+  },
+  {
+    "revision": "3cb2dba6544891f8faf5",
+    "url": "/vhbu/static/js/2.39f2a28e.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/vhbu/static/js/2.39f2a28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f07b0e82c20c81d1de",
+    "url": "/vhbu/static/js/main.70522b81.chunk.js"
+  },
+  {
+    "revision": "f9abb6bd2cf6da5a305f",
+    "url": "/vhbu/static/js/runtime-main.732dc251.js"
+  }
+]);
